0a8551729ed902fcfa2283aa5bfa0a71e88d79b8
naruto1231 <45584425+naruto1231@users.noreply.github.com>
2019-10-31 16:00:41 +0800

    Create read1.php

---
+++ read1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html lang="ja">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=Shift_JIS">
+<title>ファイルから読み込んで画面に表示</title>
+</head>
+<body>
+
+<?php
+// ファイルを読み込み専用でオープンする
+$fp = fopen('read.txt', 'r');
+// ファイルから一行読み込む
+$line = fgets($fp);
+// 読み込んだ行を出力する
+print $line;
+// ファイルをクローズする
+fclose($fp);
+?>
+
+</body>
+</html>
